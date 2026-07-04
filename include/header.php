4ad1d5e3cb7e1f886ae6b27a589ecf829b940cef
sadhanapatel1998 <sadhanapatel1998@gmail.com>
2026-07-04 10:23:54 +0530

    Update website design

---
+++ include/header.php
@@ -0,0 +1,306 @@
+        <!-- offcanvas sidebar start -->
+        <div class="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvas-sidebar">
+            <!-- offcanvas header start -->
+            <div class="offcanvas-header">
+                <!-- offcanvas logo start -->
+                <div class="offcanvas-logo">
+                    <figure>
+                        <img src="assets/images/logo/logo.png" alt="offcanvas logo">
+                    </figure>
+                </div>
+                <!-- offcanvas logo emd -->
+                <!-- offcanvas close start -->
+                <button type="button" class="offcanvas-close bg-transparent" data-bs-dismiss="offcanvas"
+                    aria-label="Close"><i class="fa-solid fa-xmark"></i></button>
+                <!-- offcanvas close end -->
+            </div>
+            <!-- offcanvas header end -->
+            <!-- offcanvas body start -->
+            <div class="offcanvas-body">
+                <!-- menu outer start -->
+                <div class="mobile-menu">
+                    <!-- Here menu will come automatically via javascript / same menu as in header -->
+                </div>
+                <!-- menu outer end -->
+                <!-- offcanvas about start -->
+                <div class="offcanvas-about d-none d-xl-block">
+                    <p>There are many variations of passages available sure there majority have suffered alteration in
+                        some form by inject humour or randomised words which don't look even slightly believable.</p>
+                </div>
+                <!-- offcanvas about end -->
+                <!-- offcanvas contact start -->
+                <div class="offcanvas-contact">
+                    <!-- widget-contact start -->
+                    <div class="widget widget-contact">
+                        <!-- widget title start -->
+                        <div class="widget-title">
+                            <h3>Contact Info</h3>
+                        </div>
+                        <!-- widget title end -->
+                        <!-- widget content start -->
+                        <div class="widget-content">
+                            <!-- offcanvas cta item start -->
+                            <div class="offcanvas-cta-item">
+                                <!-- offcanvas cta list start -->
+                                <div class="offcanvas-cta-list">
+                                    <div class="offcanvas-cta-icon">
+                                        <i class="fa-solid fa-location-dot"></i>
+                                    </div>
+                                    <div class="offcanvas-cta-content">
+                                        <p>123 Serenity Lane, Suite 101, Hometown, CA 12345</p>
+                                    </div>
+                                </div>
+                                <!-- offcanvas cta list end -->
+                                <!-- offcanvas cta list start -->
+                                <div class="offcanvas-cta-list">
+                                    <div class="offcanvas-cta-icon">
+                                        <i class="fa-solid fa-envelope"></i>
+                                    </div>
+                                    <div class="offcanvas-cta-content">
+                                        <a href="mailto:info@example.com">info@example.com</a>
+                                    </div>
+                                </div>
+                                <!-- offcanvas cta list end -->
+                                <!-- offcanvas cta list start -->
+                                <div class="offcanvas-cta-list">
+                                    <div class="offcanvas-cta-icon">
+                                        <i class="fa-solid fa-phone-volume"></i>
+                                    </div>
+                                    <div class="offcanvas-cta-content">
+                                        <a href="tel:123446788">+1 234 467 88</a>
+                                    </div>
+                                </div>
+                                <!-- offcanvas cta list end -->
+                            </div>
+                            <!-- offcanvas cta item end -->
+                        </div>
+                        <!-- widget content end -->
+                    </div>
+                    <!-- widget-contact end -->
+                </div>
+                <!-- offcanvas contact end -->
+                <!-- offcanvas button wapper start -->
+                <div class="offcanvas-button-wapper">
+                    <a href="contact.html" class="theme-button style-1" aria-label="Start For Free">
+                        <span data-text="Start For Free">Start For Free</span>
+                        <i class="fa-solid fa-arrow-right"></i>
+                    </a>
+                </div>
+                <!-- offcanvas button wapper end -->
+                <!-- offcanvas social start -->
+                <div class="offcanvas-social">
+                    <!-- widget social media start -->
+                    <div class="widget widget-social-media">
+                        <!-- widget content start -->
+                        <div class="widget-content">
+                            <ul class="social-icon">
+                                <li>
+                                    <a href="#" aria-label="instagram"><i class="fa-brands fa-instagram"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#" aria-label="facebook"><i class="fa-brands fa-facebook-f"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#" aria-label="twitter"><i class="fa-brands fa-x-twitter"></i></a>
+                                </li>
+                                <li>
+                                    <a href="#" aria-label="pinterest"><i class="fa-brands fa-pinterest-p"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- widget content end -->
+                    </div>
+                    <!-- widget social media end -->
+                </div>
+                <!-- offcanvas-social end -->
+            </div>
+            <!-- offcanvas body end -->
+        </div>
+        <!-- offcanvas sidebar end -->
+
+        <!-- header start -->
+        <header class="header header-3">
+            <!-- header lower start -->
+            <div class="header-lower">
+                <div class="container-fluid">
+                    <div class="row align-items-center justify-content-between g-0">
+                        <div class="col-12">
+                            <!-- header content start -->
+                            <div class="header-content d-flex align-items-center">
+                                <!-- header left start -->
+                                <div class="header-left">
+                                    <!-- logo box start -->
+                                    <div class="logo-box">
+                                        <div class="logo">
+                                            <a href="index.html">
+                                                <figure>
+                                                    <img src="assets/images/logo/logo.png" alt="header logo">
+                                                </figure>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <!-- logo box end  -->
+                                </div>
+                                <!-- header right end -->
+
+                                <!-- header navigation start -->
+                                <div class="header-navigation d-flex align-items-center m-auto">
+                                    <!-- main menu -->
+                                    <div class="main-menu">
+                                        <nav id="mobile-menu">
+                                            <ul>
+                                                <li class="menu-thumb">
+                                                    <a class="nav-link" href="index.html">Home<i
+                                                            class="fa-solid fa-angle-down"></i></a>
+                                                    <ul class="has-homemenu">
+                                                        <li>
+                                                            <a href="index.html">
+                                                                <img src="assets/images/header/home-1.jpg"
+                                                                    alt="home one">
+                                                                Ai Saas
+                                                            </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="index-2.html">
+                                                                <img src="assets/images/header/home-2.jpg"
+                                                                    alt="home two">
+                                                                Ai Agency
+                                                            </a>
+                                                        </li>
+                                                        <li>
+                                                            <a href="index-3.html">
+                                                                <img src="assets/images/header/home-3.jpg"
+                                                                    alt="home three">
+                                                                Ai Image
+                                                            </a>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="about.html">About Us</a>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="about.html">Pages<i
+                                                            class="fa-solid fa-angle-down"></i></a>
+                                                    <ul class="submenu">
+                                                        <li><a href="about.html">About Us</a></li>
+                                                        <li class="has-dropdown">
+                                                            <a href="project.html">Project <i
+                                                                    class="fa-solid fa-chevron-right"></i></a>
+                                                            <ul class="submenu">
+                                                                <li><a href="project.html">Project</a></li>
+                                                                <li><a href="project-details.html">Project Details</a>
+                                                                </li>
+                                                            </ul>
+                                                        </li>
+                                                        <li class="has-dropdown">
+                                                            <a href="team.html">Team <i
+                                                                    class="fa-solid fa-chevron-right"></i></a>
+                                                            <ul class="submenu">
+                                                                <li><a href="team.html">Team</a></li>
+                                                                <li><a href="team-details.html">Team Details</a>
+                                                                </li>
+                                                            </ul>
+                                                        </li>
+                                                        <li><a href="pricing.html">Pricing</a></li>
+                                                        <li><a href="testimonials.html">Testimonials</a></li>
+                                                        <li><a href="image-gallery.html">Image Gallery</a></li>
+                                                        <li><a href="faq.html">Faq's</a></li>
+                                                        <li><a href="error.html">404 Error</a></li>
+                                                        <li><a href="coming-soon.html">Coming Soon</a></li>
+                                                        <li class="has-dropdown">
+                                                            <a href="login.html">Authentication <i
+                                                                    class="fa-solid fa-chevron-right"></i></a>
+                                                            <ul class="submenu">
+                                                                <li><a href="login.html">Login</a></li>
+                                                                <li><a href="sign-up.html">Sign Up</a></li>
+                                                                <li><a href="forgot-password.html">Forgot Password</a>
+                                                                </li>
+                                                            </ul>
+                                                        </li>
+                                                    </ul>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="services.html">Services<i
+                                                            class="fa-solid fa-angle-down"></i></a>
+                                                    <ul class="submenu">
+                                                        <li><a href="services.html">Services</a></li>
+                                                        <li><a href="services-details.html">Services Details</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="blog.html">Blog <i
+                                                            class="fa-solid fa-chevron-down"></i></a>
+                                                    <ul class="submenu">
+                                                        <li><a href="blog.html">Blog Grid</a></li>
+                                                        <li><a href="blog-standard.html">Blog Standard</a></li>
+                                                        <li><a href="blog-details.html">Blog Details</a></li>
+                                                    </ul>
+                                                </li>
+                                                <li class="nav-item">
+                                                    <a class="nav-link" href="contact.html">Contact Us</a>
+                                                </li>
+                                            </ul>
+                                        </nav>
+                                    </div>
+                                </div>
+                                <!-- header navigation end -->
+
+                                <!-- header right start -->
+                                <div class="header-right d-flex align-items-center gap-3">
+                                    <!-- header button -->
+                                    <div class="header-button">
+                                        <a href="contact.html" class="theme-button style-1" aria-label="Start For Free">
+                                            <span data-text="Start For Free">Start For Free</span>
+                                            <i class="fa-solid fa-arrow-right"></i>
+                                        </a>
+                                    </div>
+
+                                    <!-- button wapper -->
+                                    <div class="button-wapper">
+                                        <!-- header-search -->
+                                        <div class="header-search">
+                                            <a href="" class="search-btn" aria-label="search">
+                                                <i class="fa-solid fa-magnifying-glass"></i>
+                                            </a>
+                                        </div>
+                                        <!-- header sidebar  -->
+                                        <div class="header-sidebar">
+                                            <a class="sidebar-toggler" data-bs-toggle="offcanvas"
+                                                href="#offcanvas-sidebar" aria-label="sidebar toggler" role="button"
+                                                aria-controls="offcanvas-sidebar">
+                                                <span></span>
+                                                <span></span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- header right end -->
+                            </div>
+                            <!-- header content end -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- header lower end -->
+
+            <!-- search box start -->
+            <div id="search-box">
+                <span class="close">
+                    <i class="fa-solid fa-xmark"></i>
+                </span>
+                <div class="search-wapper">
+                    <form action="#" class="search-form" id="search-form">
+                        <div class="input-group">
+                            <input type="text" id="searchs" name="search" placeholder="Type to search"
+                                autocomplete="off" required="required">
+                        </div>
+                        <button type="submit" class="search-submit-btn" data-text="search submit">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                        </button>
+                    </form>
+                </div>
+            </div>
+            <!-- search box end -->
+        </header>
+        <!-- header end-->
